4778ffdff67564c7f36b8d28af8cc2fe63bc07a5
Ryan Nielson <ryan@norex.ca>
2012-07-16 10:05:20 -0300

    Improved code layout and brought in line with wordpress standards

--- filtration.php
+++ filtration.php
@@ -4,16 +4,16 @@
 Plugin Name: Filtration
 Plugin URI: https://github.com/RyanNielson/filtration
 Description: Filters out user-defined words from your site.
-Version: 1.0
+Version: 1.1
 Text Domain: filtration
 Author: Ryan Nielson
 Author URI: https://github.com/RyanNielson
 */
 
-add_action('admin_menu', 'filt_add_options_page');
-add_action('plugins_loaded', 'filt_add_filters');
-add_action('admin_init', 'filt_init');
-register_uninstall_hook(__FILE__, 'filt_delete_options');
+add_action( 'admin_menu', 'filt_add_options_page' );
+add_action( 'plugins_loaded', 'filt_add_filters' );
+add_action( 'admin_init', 'filt_init' );
+register_uninstall_hook( __FILE__, 'filt_delete_options' );
 
 function filt_add_options_page() {
     add_options_page(
@@ -33,39 +33,33 @@ function filt_init() {
     );
 }
 
-/**
- * Settings validation function.  This is passed as the third argument of 
- * `register_setting`.  You can make sure all your options are okay here
- */
-function filt_settings_cleaner($in) {
+function filt_settings_cleaner( $in ) {
     $out = array();
 
-    // keywords options
-    $kws = array('filter_nonstrict_keywords', 'filter_strict_keywords');
-    foreach($kws as $kw) {
-        if(isset($in[$kw])) {
-            $kw_arr = explode(',', $in[$kw]);
-            array_walk($kw_arr, 'esc_attr');
-            array_walk($kw_arr, 'trim');
-            $out[$kw] = implode(',', $kw_arr);
+    $kws = array( 'filter_nonstrict_keywords', 'filter_strict_keywords' );
+    foreach( $kws as $kw ) {
+        if( isset($in[$kw]) ) {
+            $kw_arr = explode(',', $in[$kw] );
+            array_walk( $kw_arr, 'esc_attr' );
+            array_walk( $kw_arr, 'trim' );
+            $out[$kw] = implode( ',', $kw_arr );
         } else {
             $out[$kw] = '';
         }
     }
 
-    // checkboxes
     $checks = array(
         'filter_post_title',
         'filter_post_content',
         'filter_comments'
     );
-    foreach($checks as $c) {
-        $out[$c] = isset($in[$c]) && $in[$c] ? 1 : 0;
+    foreach( $checks as $c ) {
+        $out[$c] = isset( $in[$c] ) && $in[$c] ? 1 : 0;
     }
 
     // make sure the filter character is one character
-    if(isset($in['filter_character'])) {
-        $out['filter_character'] = esc_attr(substr($in['filter_character'], 0, 1));
+    if( isset($in['filter_character']) ) {
+        $out['filter_character'] = esc_attr( substr( $in['filter_character'], 0, 1 ) );
     }
 
     return $out;
@@ -73,68 +67,69 @@ function filt_settings_cleaner($in) {
 
 
 function filt_render_options_page() {
-    $options = get_option('filt_options');
-    $non_strict = isset($options['filter_nonstrict_keywords']) ? $options['filter_nonstrict_keywords'] : '';
-    $strict = isset($options['filter_strict_keywords']) ? $options['filter_strict_keywords'] : '';
-    $filter_char = isset($options['filter_character']) ? $options['filter_character'] : '';
+    $options = get_option( 'filt_options' );
+    $non_strict = isset( $options['filter_nonstrict_keywords'] ) ? $options['filter_nonstrict_keywords'] : '';
+    $strict = isset( $options['filter_strict_keywords'] ) ? $options['filter_strict_keywords'] : '';
+    $filter_char = isset( $options['filter_character'] ) ? $options['filter_character'] : '';
+
     ?>
     <div class="wrap">
         <?php screen_icon(); ?>
-        <h2><?php esc_html_e('Filtration Options', 'filtration'); ?></h2>
+        <h2><?php esc_html_e( 'Filtration Options', 'filtration' ); ?></h2>
 
         <form method="post" action="options.php">
-            <?php settings_fields('filt_plugin_options'); ?>
+            <?php settings_fields( 'filt_plugin_options' ); ?>
             <table class="form-table">
                 <tr>
                     <th scope="row">
-                        <?php esc_html_e('Non-strict Words', 'filtration'); ?>
+                        <?php esc_html_e( 'Non-strict Words', 'filtration'); ?>
                         <br/>
                         <span class="description">
-                            <?php esc_html_e("Words that will be censored if they're on their own. They must be provided in a comma seperated format.", 'filtration'); ?>
+                            <?php esc_html_e( "Words that will be censored if they're on their own. They must be provided in a comma seperated format.", 'filtration' ); ?>
                         </span>
                     </th>
                     <td>
-                        <textarea name="filt_options[filter_nonstrict_keywords]" rows="10" cols="60"><?php echo esc_textarea($non_strict); ?></textarea>
+                        <textarea name="filt_options[filter_nonstrict_keywords]" rows="10" cols="60"><?php echo esc_textarea( $non_strict ); ?></textarea>
                     </td>
                 </tr>
                 <tr>
                     <th scope="row">
-                        <?php esc_html_e('Strict Words', 'filtration'); ?>
+                        <?php esc_html_e( 'Strict Words', 'filtration' ); ?>
                         <br/>
                         <span class="description">
-                            <?php esc_html_e('Words that will be censored, no matter where they appear in the text. Must be provided in a comma seperated format.', 'filtration'); ?>
+                            <?php esc_html_e( 'Words that will be censored, no matter where they appear in the text. Must be provided in a comma seperated format.', 'filtration' ); ?>
                         </span>
                     </th>
                     <td>
-                        <textarea name="filt_options[filter_strict_keywords]" rows="10" cols="60"><?php echo esc_textarea($strict); ?></textarea><br />
+                        <textarea name="filt_options[filter_strict_keywords]" rows="10" cols="60"><?php echo esc_textarea( $strict ); ?></textarea><br />
                     </td>
                 </tr>
                 <tr valign="top">
                     <th scope="row"><?php esc_html_e('Filtered content', 'filtration'); ?></th>
                     <td>
-                        <input id="filter-post-content" name="filt_options[filter_post_content]" type="checkbox" value="1" <?php if (isset($options['filter_post_content'])) { checked('1', $options['filter_post_content']); } ?> /> 
-                        <label for="filter-post-content"><?php esc_html_e('Post/Page Content', 'filtration'); ?></label>
+                        <input id="filter-post-content" name="filt_options[filter_post_content]" type="checkbox" value="1" <?php if ( isset( $options['filter_post_content'] ) ) { checked( '1', $options['filter_post_content'] ); } ?> /> 
+                        <label for="filter-post-content"><?php esc_html_e( 'Post/Page Content', 'filtration' ); ?></label>
                         <br />
 
-                        <input id="filter-post-title" name="filt_options[filter_post_title]" type="checkbox" value="1" <?php if (isset($options['filter_post_title'])) { checked('1', $options['filter_post_title']); } ?> /> 
-                        <label for="filter-post-title"><?php esc_html_e('Post/Page Titles', 'filtration'); ?></label>
+                        <input id="filter-post-title" name="filt_options[filter_post_title]" type="checkbox" value="1" <?php if ( isset( $options['filter_post_title'] ) ) { checked( '1', $options['filter_post_title'] ); } ?> /> 
+                        <label for="filter-post-title"><?php esc_html_e( 'Post/Page Titles', 'filtration' ); ?></label>
                         <br />
 
-                        <input id="filter-comments" name="filt_options[filter_comments]" type="checkbox" value="1" <?php if (isset($options['filter_comments'])) { checked('1', $options['filter_comments']); } ?> /> 
-                        <label for="filter-comments"><?php esc_html_e('Comments', 'filtration'); ?></label>
+                        <input id="filter-comments" name="filt_options[filter_comments]" type="checkbox" value="1" <?php if ( isset( $options['filter_comments'] ) ) { checked( '1', $options['filter_comments'] ); } ?> /> 
+                        <label for="filter-comments"><?php esc_html_e( 'Comments', 'filtration' ); ?></label>
                         <br /><br />
                     </td>
                 </tr>
                 <tr>
                 <th scope="row">
-                        <?php esc_html_e('Filter Character', 'filtration'); ?>
+                        <?php esc_html_e( 'Filter Character', 'filtration' ); ?>
                         <br/>
                         <span class="description">
-                            <?php esc_html_e('The character that will replace the letters in the filtered word.', 'filtration'); ?>
+                            <?php esc_html_e( 'The character that will replace the letters in the filtered word.', 'filtration' ); ?>
                         </span>
                     </th>
                     <td>
-                        <input name="filt_options[filter_character]" type="text" value="<?php echo esc_attr($filter_char); ?>" />
+                        <input name="filt_options[filter_character]" type="text" value="<?php echo esc_attr( $filter_char ); ?>" />
                     </td>
                 </tr>
             </table>
@@ -148,57 +143,57 @@ function filt_render_options_page() {
 }
 
 function filt_add_filters() {
-    $options = get_option('filt_options');
+    $options = get_option( 'filt_options' );
     
-    if (isset($options['filter_post_content']) && $options['filter_post_content'] == '1')
-        add_filter('the_content', 'filt_filter_text');
-    if (isset($options['filter_post_title']) && $options['filter_post_title'] == '1')
-        add_filter('the_title', 'filt_filter_text');
-    if (isset($options['filter_comments']) && $options['filter_comments'] == '1')
-        add_filter('comment_text', 'filt_filter_text');
+    if ( isset($options['filter_post_content']) && $options['filter_post_content'] == '1' )
+        add_filter( 'the_content', 'filt_filter_text' );
+    if ( isset($options['filter_post_title']) && $options['filter_post_title'] == '1' )
+        add_filter( 'the_title', 'filt_filter_text' );
+    if ( isset($options['filter_comments']) && $options['filter_comments'] == '1' )
+        add_filter( 'comment_text', 'filt_filter_text' );
 }
 
-function filt_filter_text($text) {
+function filt_filter_text( $text ) {
     $options = get_option('filt_options');
 
     $filter_character = $options['filter_character'];
-    $strict_replacement_keywords = explode(',', $options['filter_strict_keywords']);
-    $nonstrict_replacement_keywords = explode(',', $options['filter_nonstrict_keywords']);
+    $strict_replacement_keywords = explode( ',', $options['filter_strict_keywords'] );
+    $nonstrict_replacement_keywords = explode( ',', $options['filter_nonstrict_keywords'] );
 
-    array_walk($strict_replacement_keywords, 'filt_trim_keywords');
-    array_walk($nonstrict_replacement_keywords, 'filt_trim_keywords');
+    array_walk( $strict_replacement_keywords, 'filt_trim_keywords' );
+    array_walk( $nonstrict_replacement_keywords, 'filt_trim_keywords' );
     
     // Remove duplicate keywords.
-    $strict_replacement_keywords = array_unique($strict_replacement_keywords);
-    $nonstrict_replacement_keywords = array_unique($nonstrict_replacement_keywords);
+    $strict_replacement_keywords = array_unique( $strict_replacement_keywords );
+    $nonstrict_replacement_keywords = array_unique( $nonstrict_replacement_keywords );
 
     // Replace strict keywords.
-    foreach($strict_replacement_keywords as $keyword) {
-        $replacement = str_repeat($filter_character, strlen($keyword));
-        $text = str_ireplace($keyword, $replacement, $text);
+    foreach( $strict_replacement_keywords as $keyword ) {
+        $replacement = str_repeat( $filter_character, strlen( $keyword ) );
+        $text = str_ireplace( $keyword, $replacement, $text );
     }
 
     // Replace non-strict keywords
-    foreach($nonstrict_replacement_keywords as $keyword) {
-        $replacement = str_repeat($filter_character, strlen($keyword));
-        $text = filt_str_ireplace_nonstrict($keyword, $replacement, $text);
+    foreach( $nonstrict_replacement_keywords as $keyword ) {
+        $replacement = str_repeat( $filter_character, strlen( $keyword ) );
+        $text = filt_str_ireplace_nonstrict( $keyword, $replacement, $text );
     }
    
     return $text;
 }
 
-function filt_trim_keywords(&$item) {
-    $item = trim($item);
+function filt_trim_keywords( &$item ) {
+    $item = trim( $item );
 }
 
 function filt_delete_options() {
-    delete_option('filt_options');
+    delete_option( 'filt_options' );
 }
 
 // Replace the haystack with the replacement if the needle is on its own.
-function filt_str_ireplace_nonstrict($needle, $replacement, $haystack) {
+function filt_str_ireplace_nonstrict( $needle, $replacement, $haystack ) {
     return preg_replace(
-        sprintf('/\b%s\b/i', preg_quote($needle, '/')),
+        sprintf( '/\b%s\b/i', preg_quote( $needle, '/' ) ),
         $replacement,
         $haystack
     );
